9e6ec1aefa2ba004d8e35241a466c5d617351707
1125bid <dlfdlfdldh1125@naver.com>
2024-11-21 21:06:25 +0900

    feat/give_submit UI 완료

--- lib/screens/give/give_submit.dart
+++ lib/screens/give/give_submit.dart
@@ -5,27 +5,65 @@ class GiveSubmit extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
       child: Scaffold(
         appBar: AppBar(
           actions: [Text('등록')],
         ),
-        body: Column(
-          children: [
-            Container(
-              decoration:
-                  BoxDecoration(border: Border.all(color: Color(0xffCCCCCC))),
-              height: 50,
-              width: 50,
-              child: Icon(Icons.photo_camera),
-            ),
-            Column(
-              children: [Text('제목'), TextField()],
-            )
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Color(0xffCCCCCC))),
+                height: 50,
+                width: 50,
+                child: Icon(Icons.photo_camera),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    SizedBox(height: 20),
+                    inputInfo('제목', '도와줄 수 있는 내용을 입력해주세요.'),
+                    SizedBox(height: 20),
+                    inputInfo('가격', '제공할 재능 이용원의 가격을 적어주세요.'),
+                    SizedBox(height: 20),
+                    inputInfo('상세설명', '제공할 재능의 상세 내용을 적어주세요.'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget inputInfo(String title, String text) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('$title'),
+        Container(
+          height: 50,
+          child: TextField(
+            expands: true,
+            maxLines: null,
+            minLines: null,
+            style: TextStyle(fontSize: 10),
+            decoration: InputDecoration(
+              hintText: '$text',
+
+              border: OutlineInputBorder(), //외곽선
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
