6b22902941c82f96a37e64abdb05c24f418353c4
seonghoon-02 <tutle02@gmail.com>
2024-11-22 11:08:23 +0900

    feat: mypage 메인화면 기초 ui 구현

--- lib/screens/mypage/mypage_screen.dart
+++ lib/screens/mypage/mypage_screen.dart
@@ -1,10 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:help_me/screens/mypage/mypage_give_list.dart';
+import 'mypage_give_list.dart';
 
 class MypageScreen extends StatelessWidget {
   const MypageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text('마이페이지',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            height: 100,
+            decoration: BoxDecoration(
+                color: Colors.pinkAccent,
+                borderRadius: BorderRadius.circular(10)),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '재능이',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('요청한 재능',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold)),
+                            Text('담은 재능',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold))
+                          ],
+                        ),
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(' : ',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                )),
+                            Text(' : ',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ))
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text('2개',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                )),
+                            Text('2개',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ))
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            height: 400,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.pinkAccent,
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      // Navigator.push(context,
+                      //     MaterialPageRoute(builder: (context) {
+                      //   return MypageGive();
+                      // }));
+                    },
+                    child: myPageListContainerUi('내가 담은 재능')),
+                myPageListContainerUi('내가 요청한 재능')
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Container myPageListContainerUi(String title) {
+    return Container(
+      height: 50,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          border: Border(
+        bottom: BorderSide(
+          // 밑변에 선 추가
+          color: Colors.grey[300]!,
+          width: 1, // 선의 두께
+        ),
+      )),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(title,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
   }
 }
