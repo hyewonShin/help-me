a3da2a48948391dcf8ed57b27d52dab27a750f9a
1125bid <dlfdlfdldh1125@naver.com>
2024-11-22 17:07:56 +0900

    feat:이미지 픽커 기능, 사진 입력하면 사진 나오도록 하기

--- lib/screens/give/give_submit.dart
+++ lib/screens/give/give_submit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:help_me/constant/colors.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
@@ -17,10 +18,15 @@ class _GiveSubmitState extends State<GiveSubmit> {
       TextEditingController(); //가격 textfield 데이터 받기
   TextEditingController controllText =
       TextEditingController(); //상세내용 textfiled 데이터 받기
+  XFile? file;
   Future<void> getImagePickerData() async {
-    final ImagePicker picker = ImagePicker();
-    final List<XFile> images = await picker.pickMultiImage();
-    print(images.length);
+    ImagePicker().pickImage(source: ImageSource.gallery).then((image) {
+      if (image != null) {
+        setState(() {
+          file = image;
+        });
+      }
+    });
   } //image picker
 
   @override
@@ -31,7 +37,13 @@ class _GiveSubmitState extends State<GiveSubmit> {
       },
       child: Scaffold(
         appBar: AppBar(
-          actions: [Text('등록')], //TODO GESTUREDETECTOR JSON 파일에 등록
+          actions: [
+            GestureDetector(
+                onTap: () {
+                  if (controllTitle.text == null) {}
+                },
+                child: Text('등록'))
+          ], //TODO GESTUREDETECTOR JSON 파일에 등록
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
@@ -40,7 +52,7 @@ class _GiveSubmitState extends State<GiveSubmit> {
             children: [
               GestureDetector(
                 onTap: () {
-                  getImagePickerData(); //image picker
+                  getImagePickerData(); //TODO image picker 이미지 선택하면 이미지 넣기
                 },
                 child: Container(
                   decoration: BoxDecoration(
@@ -85,7 +97,7 @@ class _GiveSubmitState extends State<GiveSubmit> {
             // minLines: null,
             style: TextStyle(fontSize: 10),
             decoration: InputDecoration(
-              hintText: '$text',
+              hintText: ('$text'),
               border: OutlineInputBorder(), //외곽선
             ),
           ),
@@ -125,3 +137,48 @@ class _GiveSubmitState extends State<GiveSubmit> {
 }
 
 //shared_preferences -핸드폰내부로 저장 할 수 있도록
+//json decode return map key dynamic rpg 게임에서 파일 저장하도록
+//상태관리 data어떻게 불러 와서 어떻게&어디서(목록위치) 관리할지? 문의→ 신혜원님
+// 등록하기에서 json 바로 수정하면 홈페이지 동기화 반영이 안됨.(새로고침, 데이터 동기화 되게 구조를 짜거나?)
+
+
+// Row(
+//   children: [
+//     SizedBox(width: 50),
+//     Expanded(child: GetBuilder<ThreadFeedWriteController>(
+//       builder: (controller) {
+//         if (controller.selectedImages == null ||
+//             (controller.selectedImages?.isEmpty ?? true)) {
+//           return Container();
+//         }
+//         return SizedBox(
+//           height: 250,
+//           child: PageView(
+//             padEnds: false,
+//             pageSnapping: false,
+//             controller: PageController(viewportFraction: 0.4),
+//             children: List.generate(
+//               controller.selectedImages?.length ?? 0,
+//               (index) => Padding(
+//                 padding: const EdgeInsets.all(4.0),
+//                 child: ClipRRect(
+//                   borderRadius: BorderRadius.circular(8),
+//                   child: Stack(children: [
+//                     Image.file(
+//                       File(controller.selectedImages![index].path),
+//                     ),
+//                     Positioned(
+//                       right: 5,
+//                       top: 5,
+//                       child: Icon(Icons.close),
+//                     )
+//                   ]),
+//                 ),
+//               ),
+//             ).toList(),
+//           ),
+//         );
+//       },
+//     )),
+//   ],
+// ),
