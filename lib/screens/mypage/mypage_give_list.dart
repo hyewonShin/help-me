2230bdd97fa3e5c25b9037029aecc85794563b27
seonghoon-02 <tutle02@gmail.com>
2024-11-23 16:13:01 +0900

    feat: figma 버젼 UI 적용

--- lib/screens/mypage/mypage_give_list.dart
+++ lib/screens/mypage/mypage_give_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:help_me/screens/mypage/dataReader.dart';
 
 class MypageGiveList extends StatelessWidget {
   const MypageGiveList({super.key});
@@ -6,39 +7,37 @@ class MypageGiveList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('내가 담은 재능'),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          SizedBox(
-            height: 29,
+        appBar: AppBar(
+          title: const Text(
+            '내가 담은 재능',
+            style: TextStyle(
+                color: Color(0xFF222222),
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                height: 0.06),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Container(
-              height: 550,
-              decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ListView.builder(
-                  itemCount: 20, //data 길이만큼으로 수정할 것
-                  itemBuilder: (BuildContext context, int index) {
-                    return buildContainerList(index);
-                  }),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            height: 650,
+            decoration: BoxDecoration(
+              color: Colors.white,
             ),
-          )
-        ],
-      ),
-    );
+            child: ListView.builder(
+                itemCount: 20, //data 길이만큼으로 수정할 것
+                itemBuilder: (BuildContext context, int index) {
+                  return buildContainerList(index);
+                }),
+          ),
+        ));
   }
 
   Container buildContainerList(int index) {
     return Container(
-      height: 70,
+      height: 153,
       width: double.infinity,
       decoration: BoxDecoration(
           border: Border(
@@ -48,49 +47,95 @@ class MypageGiveList extends StatelessWidget {
           width: 1, // 선의 두께
         ),
       )),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 45,
-                height: 45,
-                decoration: BoxDecoration(color: Colors.white),
-              ),
-              SizedBox(
-                width: 230,
+      child: Row(
+        children: [
+          Container(
+            width: 111,
+            height: 113,
+            decoration: BoxDecoration(color: Colors.amber),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
+            child: SizedBox(
+              width: 251,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('$index수영가르쳐드립니다.',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
                     Row(
                       children: [
-                        Icon(Icons.remove),
-                        Text('2',
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold)), //수량넣기
-                        Icon(Icons.add),
+                        Text(
+                          '수영가르쳐드립니다.',
+                          style: TextStyle(
+                            color: Color(0xFF222222),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(Icons.close),
                       ],
                     ),
+                    Text(
+                      '김대성',
+                      style: TextStyle(
+                        color: Color(0xFF9E9E9E),
+                        fontSize: 14,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      '1회 20,000원',
+                      style: TextStyle(
+                        color: Color(0xFF17B36F),
+                        fontSize: 16,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          width: 79,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(
+                                Icons.indeterminate_check_box,
+                                color: Color(0xFFD9D9D9),
+                              ),
+                              Text('2',
+                                  style: TextStyle(
+                                      color: Color(0xFF44D596),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold)), //수량넣기
+                              Icon(
+                                Icons.add_box,
+                                color: Color(0xFFD9D9D9),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text('40,000원',
+                            style: TextStyle(
+                              color: Color(0xFF222222),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ))
+                      ],
+                    )
                   ],
                 ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [Icon(Icons.close), Text('40,000원')],
-              ),
-              SizedBox()
-            ],
-          ),
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/screens/mypage/mypage_screen.dart
+++ lib/screens/mypage/mypage_screen.dart
@@ -1,153 +1,150 @@
 import 'package:flutter/material.dart';
 import 'package:help_me/screens/mypage/mypage_ask_list.dart';
 import 'package:help_me/screens/mypage/mypage_give_list.dart';
+import 'dart:convert';
 
 class MypageScreen extends StatelessWidget {
   const MypageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Text('마이페이지',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('마이페이지',
+              style: TextStyle(
+                color: Color(0xFF222222),
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              )),
+          SizedBox(
+            height: 20,
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Container(
-            height: 100,
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                borderRadius: BorderRadius.circular(10)),
+          Container(
+            width: 366,
+            height: 90,
+            decoration: ShapeDecoration(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(width: 1, color: Color(0xFFD9D9D9)),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('김대성',
+                      style: TextStyle(
+                        color: Color(0xFF222222),
+                        fontSize: 16,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w700,
+                      )),
+                  Row(
+                    children: [
+                      Text('재능 담기',
+                          style: TextStyle(
+                            color: Color(0xFF9E9E9E),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          )),
+                      Text(' 2회 ',
+                          style: TextStyle(
+                            color: Color(0xFF44D596),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          )),
+                      Container(
+                          width: 1,
+                          height: 22,
+                          decoration: BoxDecoration(color: Color(0xFFD9D9D9))),
+                      Text(' 재능 요청',
+                          style: TextStyle(
+                            color: Color(0xFF9E9E9E),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          )),
+                      Text(' 3회 ',
+                          style: TextStyle(
+                            color: Color(0xFF44D596),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          )),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            '나의 거래',
+            style: TextStyle(
+              color: Color(0xFF222222),
+              fontSize: 16,
+              fontFamily: 'Pretendard',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return MypageGiveList();
+              }));
+            },
             child: Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(25)),
-                  ),
+                Icon(Icons.list),
+                SizedBox(
+                  width: 12,
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '재능이',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      children: [
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('담은 재능',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold)),
-                            Text('요청한 재능',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold))
-                          ],
-                        ),
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(' : ',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                )),
-                            Text(' : ',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                ))
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('2개',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                )),
-                            Text('2개',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                ))
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                )
+                Text('내가 담은 재능',
+                    style: TextStyle(
+                      color: Color(0xFF222222),
+                      fontSize: 16,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w500,
+                    ))
               ],
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Container(
-            height: 400,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
+          SizedBox(
+            height: 12,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return MypageAskList();
+              }));
+            },
+            child: Row(
               children: [
-                const SizedBox(
-                  height: 20,
+                Icon(Icons.search),
+                SizedBox(
+                  width: 12,
                 ),
-                GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return MypageGiveList();
-                      }));
-                    },
-                    child: myPageListContainerUi('내가 담은 재능')),
-                GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return MypageAskList();
-                      }));
-                    },
-                    child: myPageListContainerUi('내가 요청한 재능'))
+                Text('내가 요청한 재능',
+                    style: TextStyle(
+                      color: Color(0xFF222222),
+                      fontSize: 16,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w500,
+                    ))
               ],
             ),
-          ),
-        )
-      ],
-    );
-  }
-
-  Container myPageListContainerUi(String title) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      decoration: BoxDecoration(
-          border: Border(
-        bottom: BorderSide(
-          // 밑변에 선 추가
-          color: Colors.grey[300]!,
-          width: 1, // 선의 두께
-        ),
-      )),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Text(title,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-        ),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
