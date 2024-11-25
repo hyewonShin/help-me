6bc1e6df5d4376fae788a50d6b30f6748593640c
Yujin <yujin.mar.chung@gmail.com>
2024-11-25 15:51:26 +0900

    feat: 정수 Price를 원화 표기법으로 변환하는 wonCurrency() 함수 추가

--- lib/screens/ask/ask_screen.dart
+++ lib/screens/ask/ask_screen.dart
@@ -3,8 +3,15 @@ import 'package:help_me/constant/colors.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 
+String wonCurrency(int price) {
+  return "${price.toString().replaceAllMapped(
+        RegExp(r'(\d)(?=(\d{3})+(?!\d))'),
+        (match) => '${match[1]},',
+      )} 원";
+}
+
 class AskScreen extends StatefulWidget {
-  AskScreen({super.key});
+  const AskScreen({super.key});
 
   @override
   State<AskScreen> createState() => _AskScreenState();
@@ -12,7 +19,11 @@ class AskScreen extends StatefulWidget {
 
 class _AskScreenState extends State<AskScreen> {
   String askJsonUrl = "lib/mock_data/ask.json";
-  List<dynamic> _data = [];
+  List<dynamic> _askData = [];
+
+  String? title;
+  String? user_id; //TODO: 사용자 이름으로 변경해야 할 것 같습니다.
+  int? price;
 
   @override
   void initState() {
@@ -24,36 +35,66 @@ class _AskScreenState extends State<AskScreen> {
     try {
       final String response = await rootBundle.loadString(url);
       final data = json.decode(response);
-      print(data);
       setState(() {
-        _data = data;
+        _askData = data;
       });
     } catch (e) {
       print('error: $e');
     }
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("HELPME")),
-        body: _data.isEmpty
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: _data.length,
-                itemBuilder: (context, index) {
-                  final item = _data[index];
-                  return ListTile(
-                    title: Text(
-                      item['title'],
-                      style: TextStyle(color: AppColors.black),
-                    ),
-                    subtitle: Column(children: [
-                      Text(item['desc']),
-                      // Text(item['user_id'].toString()),
-                      // Text(item['ask_id'].toString()),
-                    ]),
-                  );
-                },
-              ));
+        backgroundColor: AppColors.white,
+        body: Padding(
+            padding: const EdgeInsets.all(7.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 17),
+                child: SizedBox(
+                  width: double.infinity,
+                  //height: 20,
+                  child: Text(
+                    '재능요청',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _askData.length,
+                    itemBuilder: (context, index) {
+                      final item = _askData[index];
+                      return SizedBox(
+                        width: 320,
+                        height: 110,
+                        child: ListTile(
+                          title: Text(item['title'],
+                              style: TextStyle(
+                                  fontSize: 16, color: AppColors.black)),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(item['user_id'].toString(),
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  )),
+                              Text("사례금 ${wonCurrency(item['price'])}",
+                                  style: TextStyle(
+                                      color: AppColors.darkGreen,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ])));
   }
 }
