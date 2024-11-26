8aaa558cfdc08dbc606281f50e896c35b95e3a7a
seonghoon-02 <tutle02@gmail.com>
2024-11-26 16:00:08 +0900

    feat: json data 로드 방식 변경으로 코드 변경.

--- lib/screens/mypage/mypage_give_list.dart
+++ lib/screens/mypage/mypage_give_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'data_service.dart';
 import 'models.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:help_me/util/load_data_from_document.dart';
 
 class MypageGiveList extends StatefulWidget {
   const MypageGiveList({super.key});
@@ -11,10 +12,13 @@ class MypageGiveList extends StatefulWidget {
 }
 
 class _MypageGiveListState extends State<MypageGiveList> {
-  List<GiveCartList> giveList = []; // 데이터를 저장할 리스트
+  List<int> giveIdList = []; // List<GiveCartList> 타입으로 수정
   List<Users> usersList = [];
   DataService dataService = DataService();
   int userLoginId = 0; // 로그인한 사용자 ID
+  List<dynamic> gives = [];
+  List<dynamic> users = [];
+  String userName = '';
 
   @override
   void initState() {
@@ -23,148 +27,151 @@ class _MypageGiveListState extends State<MypageGiveList> {
   }
 
   Future<void> loadData() async {
-    try {
-      final gives = await dataService.loadGives();
-      final users = await dataService.loadUsers();
-      final giveCartData =
-          await dataService.createGiveCartList(gives, users, userLoginId);
+    gives = await loadDataFromDocument("give.json");
+    users = await loadDataFromDocument("users.json");
 
-      setState(() {
-        giveList = giveCartData;
-        usersList = users;
-      });
-    } catch (e) {
-      print('데이터 로드 에러: $e');
-    }
+    setState(() {
+      // 로그인한 사용자 ID에 맞는 'give' 데이터를 필터링하여 가져옴
+      giveIdList = dataService.findGiveIdsByUserId(users, userLoginId);
+      usersList = dataService.convertDynamicListToUsersList(users);
+    });
+
+    print('giveList length: ${giveIdList.length}');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            '내가 담은 재능',
-            style: TextStyle(
-              color: Color(0xFF222222),
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-            ),
+      appBar: AppBar(
+        title: const Text(
+          '내가 담은 재능',
+          style: TextStyle(
+            color: Color(0xFF222222),
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
           ),
-          centerTitle: true,
         ),
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Container(
-                height: 650,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: ListView.builder(
-                    itemCount: giveList.length, //data 길이만큼 리스트 뷰 생성
-                    itemBuilder: (BuildContext context, int index) {
-                      return buildContainerList(index);
-                    }),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              height: 650,
+              decoration: const BoxDecoration(
+                color: Colors.white,
               ),
-              Container(
-                width: 370,
-                height: 42,
-                decoration: ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF44D596),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
-
-                    ///버튼 터치시 호출할 함수
-                    onPressed: () {
-                      showCupertinoDialog(
-                          context: context,
-                          builder: (context) {
-                            return CupertinoAlertDialog(
-                              title: Text('모두 구매하시겠습니까?'),
-                              content: Text('금액 : 원'),
-                              actions: [
-                                CupertinoDialogAction(
-                                  isDefaultAction: true,
-                                  onPressed: () {
-                                    //취소 버튼 터치시 알림창 종료
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text('취소'),
-                                ),
-                                CupertinoDialogAction(
-                                  isDestructiveAction: true,
-                                  onPressed: () {
-                                    // 스택에서 현재 페이지 포함 3번째 페이지로 이동
-                                    int count = 0;
-                                    Navigator.of(context).popUntil((route) {
-                                      count++;
-                                      return count == 3;
-                                    });
-                                  },
-                                  child: Text('확인'),
-                                ),
-                              ],
-                            );
-                          });
+              child: ListView.builder(
+                itemCount: giveIdList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return buildContainerList(index); // giveList[index] 사용
+                },
+              ),
+            ),
+            Container(
+              width: 370,
+              height: 42,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF44D596),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {
+                  showCupertinoDialog(
+                    context: context,
+                    builder: (context) {
+                      return CupertinoAlertDialog(
+                        title: Text('모두 구매하시겠습니까?'),
+                        content: Text('금액 : 원'),
+                        actions: [
+                          CupertinoDialogAction(
+                            isDefaultAction: true,
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('취소'),
+                          ),
+                          CupertinoDialogAction(
+                            isDestructiveAction: true,
+                            onPressed: () {
+                              int count = 0;
+                              Navigator.of(context).popUntil((route) {
+                                count++;
+                                return count == 3;
+                              });
+                            },
+                            child: Text('확인'),
+                          ),
+                        ],
+                      );
                     },
-                    child: Text(
-                      '모두 구매 하기',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    )),
-              )
-            ],
-          ),
-        ));
+                  );
+                },
+                child: Text(
+                  '모두 구매 하기',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Container buildContainerList(int index) {
-    //index를 파라미터로 받아 Container를 생성하는 함수
-    GiveCartList give = giveList[index];
-    String imgUrl = give.image;
-    int giveListQuantity = 0;
-
-    giveListQuantity =
-        dataService.getGiveQuantity(usersList, userLoginId, give.giveId);
+    int price = dataService.findPriceByGiveId(gives, giveIdList[index]);
+    int quantity = dataService.findQuantityByUserIdAndGiveId(
+        users, userLoginId, giveIdList[index]);
+    int giveNameId = dataService.findNameByGiveId(gives, giveIdList[index]);
+    String userName = dataService.getNameByUserId(usersList, giveNameId);
+    String imgUrl = dataService.findImgUrlByGiveId(gives, giveIdList[index]);
+    print('ssssssssss$userName');
 
     // 수량 감소 함수
     void decreaseQuantity() {
       setState(() {
-        if (giveListQuantity > 0) {
-          giveListQuantity--; // 값 1 감소
+        if (quantity > 0) {
+          quantity--; // 값 1 감소
         }
       });
     }
 
-    // 수량 증가 감소 함수
+    // 수량 증가 함수
     void increaseQuantity(bool plus) {
-      dataService.increaseQuantity(usersList, userLoginId, give.giveId, plus);
+      dataService.increaseQuantity(
+          usersList, userLoginId, giveIdList[index], plus);
       setState(() {
-        giveListQuantity =
-            dataService.getGiveQuantity(usersList, userLoginId, give.giveId);
+        quantity = dataService.getGiveQuantity(
+            usersList, userLoginId, giveIdList[index]);
       });
     }
 
+    print('${giveIdList[index]}${giveIdList[index].runtimeType}');
     return Container(
       height: 153,
       width: double.infinity,
       decoration: BoxDecoration(
-          border: Border(
-        bottom: BorderSide(
-          // 밑변에 선 추가
-          color: Colors.grey[300]!,
-          width: 1, // 선의 두께
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey[300]!,
+            width: 1,
+          ),
         ),
-      )),
+      ),
       child: Row(
         children: [
           ClipRRect(
@@ -184,7 +191,7 @@ class _MypageGiveListState extends State<MypageGiveList> {
                     Row(
                       children: [
                         Text(
-                          '${give.title}',
+                          '${dataService.findTitleByGiveId(gives, giveIdList[index])}', //***
                           style: const TextStyle(
                             color: Color(0xFF222222),
                             fontSize: 16,
@@ -192,11 +199,11 @@ class _MypageGiveListState extends State<MypageGiveList> {
                           ),
                         ),
                         const Spacer(),
-                        const Icon(Icons.close), //delete 기능 넣을것#####
+                        const Icon(Icons.close),
                       ],
                     ),
                     Text(
-                      '${give.username}',
+                      '$userName', // ***
                       style: const TextStyle(
                         color: Color(0xFF9E9E9E),
                         fontSize: 14,
@@ -204,16 +211,14 @@ class _MypageGiveListState extends State<MypageGiveList> {
                       ),
                     ),
                     Text(
-                      '1회 ${give.price}원',
+                      '1회 $price원', //***
                       style: const TextStyle(
                         color: Color(0xFF17B36F),
                         fontSize: 16,
                         fontWeight: FontWeight.w500,
                       ),
                     ),
-                    const SizedBox(
-                      height: 16,
-                    ),
+                    const SizedBox(height: 16),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -225,23 +230,24 @@ class _MypageGiveListState extends State<MypageGiveList> {
                               GestureDetector(
                                 onTap: () {
                                   increaseQuantity(false);
-                                }, // 아이콘 터치 시 감소 함수 호출
+                                },
                                 child: Icon(
-                                  Icons
-                                      .indeterminate_check_box, //+- 기능 넣기 ###########
+                                  Icons.indeterminate_check_box,
                                   color: Color(0xFFD9D9D9),
                                 ),
                               ),
-                              Text('$giveListQuantity',
-                                  style: TextStyle(
-                                      color: Color(0xFF44D596),
-                                      fontSize: 20,
-                                      fontWeight:
-                                          FontWeight.bold)), //수량넣기############
+                              Text(
+                                '$quantity', //***
+                                style: TextStyle(
+                                  color: Color(0xFF44D596),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                               GestureDetector(
                                 onTap: () {
                                   increaseQuantity(true);
-                                }, // 아이콘 터치 시 증가 함수 호출
+                                },
                                 child: Icon(
                                   Icons.add_box,
                                   color: Color(0xFFD9D9D9),
@@ -251,19 +257,20 @@ class _MypageGiveListState extends State<MypageGiveList> {
                           ),
                         ),
                         Text(
-                            '${giveListQuantity * give.price}원', //금액 변경 함수 넣기 #########
-                            style: TextStyle(
-                              color: Color(0xFF222222),
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                            ))
+                          '${quantity * price}원',
+                          style: TextStyle(
+                            color: Color(0xFF222222),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
                       ],
-                    )
+                    ),
                   ],
                 ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
